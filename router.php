bf550ad0847319840766a7a5c596550e063d212b
David Zentgraf <deceze@gmail.com>
2012-09-17 14:36:40 +0200

    Adding documentation to Router.

--- router.php
+++ router.php
@@ -12,6 +12,10 @@ class Router {
 	          $routeFactory,
 	          $defaultCallback;
 
+	/**
+	 * @param RouteFactory $routeFactory Optionally supply an instance of a RouteFactory
+	 *	that may instantiate alternative Route objects. Defaults to standard RouteFactory.
+	 */
 	public function __construct(RouteFactory $routeFactory = null) {
 		if (!$routeFactory) {
 			require_once 'route_factory.php';
@@ -20,10 +24,24 @@ class Router {
 		$this->routeFactory = $routeFactory;
 	}
 
+	/**
+	 * Create a new route using a pattern, dispatch array and callback and add it to the stack.
+	 *
+	 * @param string $pattern
+	 * @param array $dispatch
+	 * @param callable $callback
+	 */
 	public function add($pattern, array $dispatch = array(), $callback = null) {
 		$this->addRoute($this->routeFactory->newRoute($pattern, $dispatch), $callback);
 	}
 
+	/**
+	 * Add a Route object and a callback to the stack.
+	 * 
+	 * @param Route $route
+	 * @param callable $callback
+	 * @throws InvalidArgumentException if $callback is not callable
+	 */
 	public function addRoute(Route $route, $callback = null) {
 		if ($callback && !is_callable($callback)) {
 			throw new InvalidArgumentException('$callback must be of type callable, got ' . gettype($callback));
@@ -31,6 +49,12 @@ class Router {
 		$this->routes[] = compact('route', 'callback');
 	}
 
+	/**
+	 * Set a default callback for routes that have no specific callback defined.
+	 *
+	 * @param callable $callback
+	 * @throws InvalidArgumentException if $callback is not callable
+	 */
 	public function defaultCallback($callback) {
 		if ($callback && !is_callable($callback)) {
 			throw new InvalidArgumentException('$callback must be of type callable, got ' . gettype($callback));
@@ -38,6 +62,15 @@ class Router {
 		$this->defaultCallback = $callback;
 	}
 
+	/**
+	 * Start the routing process.
+	 *
+	 * @param string $url The URL to route.
+	 * @param callable $noMatch Callback in case no route matched.
+	 * @return mixed Return value of whatever callback was invoked.
+	 * @throws InvalidArgumentException if $noMatch is not callable.
+	 * @throws RuntimeException in case no route matched and no callback was supplied.
+	 */
 	public function route($url, $noMatch = null) {
 		if ($noMatch && !is_callable($noMatch)) {
 			throw new InvalidArgumentException('$noMatch must be of type callable, got ' . gettype($noMatch));
@@ -50,12 +83,18 @@ class Router {
 		}
 
 		if ($noMatch) {
-			call_user_func($noMatch, $url);
+			return call_user_func($noMatch, $url);
 		}
 		
 		throw new RuntimeException('No route matched');
 	}
 
+	/**
+	 * Get a URL from a dispatch array.
+	 *
+	 * @param array $dispatch
+	 * @return mixed Matching URL or false if no match.
+	 */
 	public function reverseRoute(array $dispatch) {
 		foreach ($this->routes as $route) {
 			if ($match = $route['route']->matchDispatch($dispatch)) {
@@ -65,6 +104,9 @@ class Router {
 		return false;
 	}
 
+	/**
+	 * Executes a callback or the default callback for a matched route.
+	 */
 	private function callback($callback, Route $route) {
 		if ($callback) {
 			if (!is_callable($callback)) {
