130b5528b4b196e9e4de0f103b5087bd5950ee2e
David Zentgraf <deceze@gmail.com>
2012-12-15 09:58:01 +0100

    Adding routing by request method as feature. Existing APIs remain unchanged and apply to GET, POST, PUT and DELETE requests. Adding additional APIs for creating routes for specific methods, including a whole host of different convenience wrappers. Documentation and tests not yet updated.

--- Router.php
+++ Router.php
@@ -8,6 +8,15 @@ use InvalidArgumentException,
 
 class Router {
 
+    const GET     = 1;
+    const POST    = 2;
+    const PUT     = 4;
+    const DELETE  = 8;
+    const HEAD    = 16;
+    const TRACE   = 32;
+    const OPTIONS = 64;
+    const CONNECT = 128;
+
     protected $routes = array(),
               $routeFactory,
               $defaultCallback;
@@ -26,27 +35,97 @@ class Router {
 
     /**
      * Create a new route using a pattern, dispatch array and callback and add it to the stack.
+     * Matches GET, POST, PUT and DELETE request methods.
      *
      * @param string $pattern
      * @param array $dispatch
      * @param callable $callback
      */
     public function add($pattern, array $dispatch = array(), $callback = null) {
-        $this->addRoute($this->routeFactory->newRoute($pattern, $dispatch), $callback);
+        $this->addMethodRoute(self::GET | self::POST | self::PUT | self::DELETE, $this->routeFactory->newRoute($pattern, $dispatch), $callback);
+    }
+    
+    /**
+     * Create a new route matching GET requests.
+     *
+     * @param string $pattern
+     * @param array $dispatch
+     * @param callable $callback
+     */
+    public function addGet($pattern, array $dispatch = array(), $callback = null) {
+        $this->addMethod(self::GET, $pattern, $dispatch, $callback);
+    }
+
+    /**
+     * Create a new route matching POST requests.
+     *
+     * @param string $pattern
+     * @param array $dispatch
+     * @param callable $callback
+     */
+    public function addPost($pattern, array $dispatch = array(), $callback = null) {
+        $this->addMethod(self::POST, $pattern, $dispatch, $callback);
+    }
+
+    /**
+     * Create a new route matching PUT requests.
+     *
+     * @param string $pattern
+     * @param array $dispatch
+     * @param callable $callback
+     */
+    public function addPut($pattern, array $dispatch = array(), $callback = null) {
+        $this->addMethod(self::PUT, $pattern, $dispatch, $callback);
+    }
+
+    /**
+     * Create a new route matching DELETE requests.
+     *
+     * @param string $pattern
+     * @param array $dispatch
+     * @param callable $callback
+     */
+    public function addDelete($pattern, array $dispatch = array(), $callback = null) {
+        $this->addMethod(self::DELETE, $pattern, $dispatch, $callback);
+    }
+
+    /**
+     * Create a new route matching a custom combination of request methods.
+     *
+     * @param int $method A bitmask of request methods.
+     * @param string $pattern
+     * @param array $dispatch
+     * @param callable $callback
+     */
+    public function addMethod($method, $pattern, array $dispatch = array(), $callback = null) {
+        $this->addMethodRoute($method, $this->routeFactory->newRoute($pattern, $dispatch), $callback);
     }
 
     /**
      * Add a Route object and a callback to the stack.
+     * Matches GET, POST, PUT and DELETE request methods.
      * 
      * @param Route $route
      * @param callable $callback
      * @throws InvalidArgumentException if $callback is not callable
      */
     public function addRoute(Route $route, $callback = null) {
+        $this->addMethodRoute(self::GET | self::POST | self::PUT | self::DELETE, $route, $callback);
+    }
+    
+    /**
+     * Add a Route object with specified request method and a callback to the stack.
+     * 
+     * @param int $method A bitmask of request methods.
+     * @param Route $route
+     * @param callable $callback
+     * @throws InvalidArgumentException if $callback is not callable
+     */
+    public function addMethodRoute($method, Route $route, $callback = null) {
         if ($callback && !is_callable($callback, true)) {
             throw new InvalidArgumentException('$callback must be of type callable, got ' . gettype($callback));
         }
-        $this->routes[] = compact('route', 'callback');
+        $this->routes[] = compact('method', 'route', 'callback');
     }
 
     /**
@@ -64,6 +143,7 @@ class Router {
 
     /**
      * Start the routing process.
+     * Matches GET, POST, PUT and DELETE requests equally.
      *
      * @param string $url The URL to route.
      * @param callable $noMatch Callback in case no route matched.
@@ -72,11 +152,43 @@ class Router {
      * @throws RuntimeException in case no route matched and no callback was supplied.
      */
     public function route($url, $noMatch = null) {
+        return $this->routeMethod(self::GET | self::POST | self::PUT | self::DELETE, $url, $noMatch);
+    }
+    
+    /**
+     * Start the routing process for a specific request method.
+     * The request method is supplied as string, i.e. can be plucked directly from $_SERVER['REQUEST_METHOD'].
+     *
+     * @param string $method The request method.
+     * @param string $url The URL to route.
+     * @param callable $noMatch Callback in case no route matched.
+     * @return mixed Return value of whatever callback was invoked.
+     * @throws InvalidArgumentException if $method is not supported or $noMatch is not callable.
+     * @throws RuntimeException in case no route matched and no callback was supplied.
+     */
+    public function routeMethodFromString($method, $url, $noMatch = null) {
+        return $this->routeMethod($this->stringToRequestMethod($method), $url, $noMatch);
+    }
+    
+    /**
+     * Start the routing process for a specific request method.
+     *
+     * @param int $method Bitmask of the request method.
+     * @param string $url The URL to route.
+     * @param callable $noMatch Callback in case no route matched.
+     * @return mixed Return value of whatever callback was invoked.
+     * @throws InvalidArgumentException if $noMatch is not callable.
+     * @throws RuntimeException in case no route matched and no callback was supplied.
+     */
+    public function routeMethod($method, $url, $noMatch = null) {
         if ($noMatch && !is_callable($noMatch, true)) {
             throw new InvalidArgumentException('$noMatch must be of type callable, got ' . gettype($noMatch));
         }
 
         foreach ($this->routes as $route) {
+            if (!($route['method'] & $method)) {
+                continue;
+            }
             if ($match = $route['route']->matchUrl($url)) {
                 return $this->callback($route['callback'], $match);
             }
@@ -91,12 +203,26 @@ class Router {
 
     /**
      * Get a URL from a dispatch array.
+     * Matches GET, POST, PUT and DELETE routes equally.
      *
      * @param array $dispatch
      * @return mixed Matching URL or false if no match.
      */
     public function reverseRoute(array $dispatch) {
+        return $this->reverseRouteMethod(self::GET | self::POST | self::PUT | self::DELETE, $dispatch);
+    }
+    
+    /**
+     * Get a URL from a dispatch array for a specific method.
+     *
+     * @param array $dispatch
+     * @return mixed Matching URL or false if no match.
+     */
+    public function reverseRouteMethod($method, array $dispatch) {
         foreach ($this->routes as $route) {
+            if (!($route['method'] & $method)) {
+                continue;
+            }
             if ($match = $route['route']->matchDispatch($dispatch)) {
                 return $match->url();
             }
@@ -120,6 +246,18 @@ class Router {
         }
 
         throw new RuntimeException(sprintf('Route %s matched URL %s, but no callback given', $route->pattern(), $route->url()));
+    }
+    
+    /**
+     * Returns matching request method constant from string representation.
+     */
+    protected function stringToRequestMethod($string) {
+        $self = new ReflectionClass($this);
+        $method = $self->getConstant(strtoupper($string));
+        if (!$method) {
+            throw new InvalidArgumentException("Unsupported request method '$string'");
+        }
+        return $method;
     }
 
 }
